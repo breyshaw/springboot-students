5bb55975a0c240434b78e74c0bc54b3cbd118b05
breyshaw <dawsonbrey@gmail.com>
2022-03-17 14:21:08 -0400

    add error handling if the student email being posted already exists in the database

--- src/main/java/com/breyshaw/demo/student/StudentRepository.java
+++ src/main/java/com/breyshaw/demo/student/StudentRepository.java
@@ -1,12 +1,14 @@
-//Repository is the naming convention for anything that accesses the database, specifically when working with JPA
 package com.breyshaw.demo.student;
-
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
+//Repository is the naming convention for anything that accesses the database, specifically when working with JPA
 
 @Repository
 public interface StudentRepository 
 extends JpaRepository<Student, Long>{
-  
-  
+  //custom function that finds user by email
+  @Query("SELECT s FROM Student s WHERE s.email = ?1") //s is an alias, this is jpql
+  Optional<Student> findStudentByEmail(String email);
 }
--- src/main/java/com/breyshaw/demo/student/StudentService.java
+++ src/main/java/com/breyshaw/demo/student/StudentService.java
@@ -1,12 +1,14 @@
 package com.breyshaw.demo.student;
+
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 //Adding the "service layer" here. The service layer is mainly responsible for business logic. 
 
-@Service 
+@Service
 public class StudentService {
 
   private final StudentRepository studentRepository;
@@ -17,10 +19,14 @@ public class StudentService {
   }
 
   public List<Student> getStudents() {
-    return studentRepository.findAll(); //Look at all those methods!
-    //Currently brings back an empty array because there are no students
-}
+    return studentRepository.findAll(); // Look at all those methods!
+  }
+
   public void addNewStudent(Student student) {
-      System.out.println(student);
-	}
+    Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+    if (studentOptional.isPresent()) {
+      throw new IllegalStateException("Email already used");
+    }
+    studentRepository.save(student);
+  }
 }

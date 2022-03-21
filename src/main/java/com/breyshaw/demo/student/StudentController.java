4edf9bebb5c599034a19451c33b0fa29b73644a2
breyshaw <dawsonbrey@gmail.com>
2022-03-21 14:21:00 -0400

    complete put/update functionality, test with postman

--- src/main/java/com/breyshaw/demo/student/StudentController.java
+++ src/main/java/com/breyshaw/demo/student/StudentController.java
@@ -1,13 +1,14 @@
 package com.breyshaw.demo.student;
-
 import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
@@ -45,5 +46,13 @@ public class StudentController {
   @DeleteMapping(path = "{studentId}")
   public void deleteStudent(@PathVariable("studentId") Long id) {
     studentService.deleteStudent(id);
+  }
+
+  @PutMapping(path = "{studentId}")
+  public void updateStudent(
+      @PathVariable("studentId") Long studentId,
+      @RequestParam(required = false) String name,
+      @RequestParam(required = false) String email) {
+    studentService.updateStudent(studentId, name, email);
   }
 }
--- src/main/java/com/breyshaw/demo/student/StudentService.java
+++ src/main/java/com/breyshaw/demo/student/StudentService.java
@@ -1,10 +1,10 @@
 package com.breyshaw.demo.student;
-
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 //Adding the "service layer" here. The service layer is mainly responsible for business logic. 
 
@@ -29,12 +29,33 @@ public class StudentService {
     }
     studentRepository.save(student);
   }
-  
+
   public void deleteStudent(Long studentId) {
     boolean exists = studentRepository.existsById(studentId);
     if (!exists) {
       throw new IllegalStateException("Student with id " + studentId + "does not exist");
     }
     studentRepository.deleteById(studentId);
+  }
+
+  @Transactional // Makes it where we do not have to use queries to the StudentRepository
+  public void updateStudent(Long studentId, 
+                            String name, 
+                            String email) {
+    Student student = studentRepository.findById(studentId) // checking if the student exists
+        .orElseThrow(() -> new IllegalStateException("student with id " + studentId + "does not exist"));
+
+    if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+      student.setName(name);
+    }
+
+    if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+      Optional<Student> studentOptional = studentRepository
+          .findStudentByEmail(email);
+      if (studentOptional.isPresent()) {
+        throw new IllegalStateException("email taken");
+      }
+      student.setEmail(email);
+    }
   }
 }

726b169e3ee45c1f986389da6e31703bf3d732a8
breyshaw <dawsonbrey@gmail.com>
2022-03-21 13:17:07 -0400

    add delete functionality, test with postman

--- src/main/java/com/breyshaw/demo/student/StudentController.java
+++ src/main/java/com/breyshaw/demo/student/StudentController.java
@@ -4,7 +4,9 @@ import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -40,4 +42,8 @@ public class StudentController {
     studentService.addNewStudent(student);
   }
 
+  @DeleteMapping(path = "{studentId}")
+  public void deleteStudent(@PathVariable("studentId") Long id) {
+    studentService.deleteStudent(id);
+  }
 }
